348c9959d52e435651e667446939da14f325cdac
dexterleng <thequicklab@gmail.com>
2018-09-09 20:01:52 +0800

    mailer init

---
+++ app/mailers/instance_mailer.rb
@@ -0,0 +1,13 @@
+class InstanceMailer < ApplicationMailer
+  # Sends an invitation email for the given invitation.
+  #
+  # @param [Instance] instance The course that was involved.
+  # @param [Instance::UserInvitation] invitation The invitation which was generated.
+  def user_invitation_email(instance, invitation)
+    @instance = instance
+    @invitation = invitation
+    @recipient = invitation
+
+    mail(to: invitation.email, subject: t('.subject', instance: @instance.name))
+  end
+end
