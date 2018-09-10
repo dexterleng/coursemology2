a55ca5cf23e18b42e09b3e1ca8cf697c6cd8931f
dexterleng <thequicklab@gmail.com>
2018-09-10 18:54:10 +0800

    setup model/instance/user_invitation.spec

--- spec/models/instance/user_invitation_spec.rb
+++ spec/models/instance/user_invitation_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Instance::UserInvitation, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#generate_invitation_key' do
+    it 'starts with "J"' do
+      expect(subject.invitation_key).to start_with('J')
+    end
+  end
 end
