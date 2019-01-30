b9a0b10d1040da8c2f882b001e8063b347466f59
dexterleng <dexterlengcs@gmail.com>
2019-01-30 14:25:48 +0800

    Fix linting

--- app/services/course/survey/survey_export_service.rb
+++ app/services/course/survey/survey_export_service.rb
@@ -3,14 +3,13 @@ require 'csv'
 
 class Course::Survey::SurveyExportService
   class << self
-
     # Converts survey to string csv format.
     #
     # @param [Course::Survey] survey The survey to be converted
     # @return [String] The survey in csv format.
     def export(survey)
       responses = Course::Survey::Response.includes(answers: [:options, :question]).where(survey: survey)
-      questions = survey.questions.sort_by { |question| question.weight }
+      questions = survey.questions.sort_by(&:weight)
       header = generate_header(questions)
 
       CSV.generate(headers: true, force_quotes: true) do |csv|
@@ -27,9 +26,9 @@ class Course::Survey::SurveyExportService
     private
 
     def generate_header(questions)
-      ["Timestamp"] + questions.map {|question| question.description }
+      ['Timestamp'] + questions.map(&:description)
     end
-  
+
     def generate_row(response, questions)
       answers_hash = response.answers.map { |answer| [answer.question_id, answer] }.to_h
       questions.map do |question|
@@ -37,18 +36,18 @@ class Course::Survey::SurveyExportService
         generate_value(answer)
       end
     end
-  
+
     def generate_value(answer)
       # Handles the case where there is no answer.
       # This happens when a question is added after the user has submitted a response.
-      return "" if answer.nil?
+      return '' if answer.nil?
 
       question = answer.question
       if question.text?
         return answer.text_response
       elsif question.multiple_choice? || question.multiple_response?
         options = answer.options.map { |option| option.question_option.option }
-        return options.join(";")
+        return options.join(';')
       else
         throw new Exception
       end
